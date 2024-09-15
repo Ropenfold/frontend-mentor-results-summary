c3337abe7d29ab7629a0c53f67636c74f0a0f3e7
stuart.roper <stuart.roper@bravissimo.com>
2024-09-15 17:10:00 +0100

    create ScoreComponent

--- app/components/ScoreComponent.js
+++ app/components/ScoreComponent.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import styles from '../components/scoreComponent.module.css'
+import Image from 'next/image';
 
-const ScoreComponent = () => {
+
+const ScoreComponent = ({item: {category, score, icon}, className, fontColour }) => {
   return (
-    <div>ScoreComponent</div>
+    <div className={styles[className]}>
+      <div className={styles.iconContainer}>
+      <Image src={icon} alt={category} width={25} height={25}/>
+      </div>
+      <div className={styles[fontColour]}>{category}</div>
+      <div className={styles.scoreContainer}>
+  <b>{score}</b><p className={styles.hundred}>&nbsp;/ 100</p>
+</div>
+    </div>
   )
 }
 
 export default ScoreComponent
+
